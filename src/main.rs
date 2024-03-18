ab3a426428d6b76cf1861279a3f07446d662bcb6
lzh.li.gz <lzh.li.gz@gmail.com>
2024-03-18 02:36:24 +0000

    modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,12 @@ use stocks::{close_order, open_order};
 
 use stocks::structs::order::Order;
 
+use std::env;
 use stocks::enums::order_types::OrderType;
+
+use rand::prelude::*;
+
+use std::str::FromStr;
 /// Adds two numbers together.
 
 ///
@@ -43,6 +48,18 @@ fn add_numbers(one: i32, two: i32) -> i32 {
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let action: &String = &args[1];
+
+    let name: &String = &args[2];
+
+    let amount: i32 = i32::from_str(&args[3]).unwrap();
+
+    let price: f32 = f32::from_str(&args[4]).unwrap();
+    let mut new_order: Order =
+        open_order(amount, OrderType::Long, &name.as_str(), price, None, None);
+
     let stock: Value = json!({
 
         "name": "MonolithAi",
@@ -52,6 +69,33 @@ fn main() {
         "history": [19.4, 26.9, 32.5]
 
     });
+
+    match action.as_str() {
+        "buy" => {
+            println!(
+                "the value of your investment is: {}",
+                new_order.current_value()
+            );
+        }
+
+        "sell" => {
+            let mut rng = rand::thread_rng();
+
+            let new_price_ref: f32 = rng.gen();
+
+            let new_price: f32 = new_price_ref * 100 as f32;
+
+            new_order.stock.update_price(new_price);
+
+            let sale_profit: f32 = close_order(new_order);
+
+            println!("here is the profit you made: {}", sale_profit);
+        }
+
+        _ => {
+            panic!("Only 'buy' and 'sell' actions are supported");
+        }
+    }
 
     println!("first price: {}", stock["history"][0]);
 
@@ -65,23 +109,23 @@ fn main() {
 
     println!("hello stocks");
 
-    let mut new_order: Order = open_order(20, OrderType::Long, "bumper", 56.8, None, None);
+    // let mut new_order: Order = open_order(20, OrderType::Long, "bumper", 56.8, None, None);
 
-    println!("the current price is: {}", &new_order.current_value());
+    // println!("the current price is: {}", &new_order.current_value());
 
-    println!("the current profit is: {}", &new_order.current_profit());
-    new_order.stock.update_price(43.1);
+    // println!("the current profit is: {}", &new_order.current_profit());
+    // new_order.stock.update_price(43.1);
 
-    println!("the current price is: {}", &new_order.current_value());
+    // println!("the current price is: {}", &new_order.current_value());
 
-    println!("the current profit is: {}", &new_order.current_profit());
+    // println!("the current profit is: {}", &new_order.current_profit());
 
-    new_order.stock.update_price(82.7);
+    // new_order.stock.update_price(82.7);
 
-    println!("the current price is: {}", &new_order.current_value());
+    // println!("the current price is: {}", &new_order.current_value());
 
-    println!("the current profit is: {}", &new_order.current_profit());
-    let profit: f32 = close_order(new_order);
+    // println!("the current profit is: {}", &new_order.current_profit());
+    // let profit: f32 = close_order(new_order);
 
-    println!("we made {} profit", profit);
+    // println!("we made {} profit", profit);
 }
